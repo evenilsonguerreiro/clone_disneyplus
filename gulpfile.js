63fe8f625dec0b7acff60c7c269c2afed6c6b716
evenilson <evenilsonguerreiro@gmail.com>
2023-12-04 08:49:43 -0300

    hero

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 
 
+
 function styles(){
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({outputStyle:'compressed'}))
@@ -16,7 +17,9 @@ function images(){
 
 }
 
-exports.default = gulp.parallel(styles,images);
+
+
+exports.default = gulp.parallel(styles, images);
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss',gulp.parallel(styles))
 }
