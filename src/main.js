e5648990c10df30111ba9e24c6c468b3c01024e8
evenilson <evenilsonguerreiro@gmail.com>
2023-12-06 14:11:47 -0300

    parte-03

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]')
 
    
     for(let i = 0; i < buttons.length; i++){
@@ -12,7 +13,21 @@ document.addEventListener('DOMContentLoaded',function(){
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    for (let i = 0; i < questions.length;i++){
+        questions[i].addEventListener('click', abreOufecha);
+    }
 })
+
+
+function abreOufecha(elemento){
+    const classe = 'faq__quetions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
+
+
 
 function removerButtonativo(){
     const buttons = document.querySelectorAll('[data-tab-button]');
@@ -28,3 +43,4 @@ function escondTodasabas(){
         tabContainer[i].classList.remove('shows__list--is-active');
     }
 }
+
