9acbe09a0bca62a0595c22eb9abdb01be01904e3
evenilson <evenilsonguerreiro@gmail.com>
2023-12-07 15:31:59 -0300

    final

--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ document.addEventListener('DOMContentLoaded',function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]')
 
+
+    const heroSection = document.querySelector('.hero')
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if(posicaoAtual < alturaHero){
+            ocultaElementodoheader();
+        } else {
+            exibeElementodoheader();
+        }
+    })
+
    
     for(let i = 0; i < buttons.length; i++){
             buttons[i].addEventListener('click',function(botao){
@@ -11,20 +25,32 @@ document.addEventListener('DOMContentLoaded',function(){
             aba.classList.add('shows__list--is-active');
             removerButtonativo();
             botao.target.classList.add('shows__tabs__button--is-active');
-        })
+        }) 
+        
+        for (let i = 0; i < questions.length; i ++){
+           questions[i].addEventListener('click', abreOufecha); 
+        }
     }
-
-    for (let i = 0; i < questions.length;i++){
-        questions[i].addEventListener('click', abreOufecha);
-    }
+   
 })
 
-
 function abreOufecha(elemento){
-    const classe = 'faq__quetions__item--is-open';
+    const classe = 'faq__questions__item--is-open';
     const elementoPai = elemento.target.parentNode;
 
     elementoPai.classList.toggle(classe);
+
+}
+
+
+function ocultaElementodoheader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is--hidden')
+}
+
+function exibeElementodoheader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is--hidden')
 }
 
 
